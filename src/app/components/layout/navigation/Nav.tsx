f0eb3efb1f331ce2d113e27cb631a2973a1e6449
Jessica MAVAKALA <mavakala.jessica@laposte.net>
2024-07-12 10:44:35 +0200

    navbar

--- src/app/components/layout/navigation/Nav.tsx
+++ src/app/components/layout/navigation/Nav.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Nav_module from "./nav.module.css";
 import Link from "next/link";
 import Image from "next/image";
-import IconBtn from "../../iconBtn/IconBtn";
+import IconBtn from "../../common/iconBtn/IconBtn";
 
 export default function Nav() {
   const links = [
@@ -17,6 +17,9 @@ export default function Nav() {
 
   return (
     <div className={Nav_module.nav}>
+      {/* si link est inferieur a 512 alors
+      modifier le menu en burger 
+      sinon rien  */}
       <div className={Nav_module.main}>
         {links.map((link: any) =>
           link.src ? (
@@ -25,7 +28,7 @@ export default function Nav() {
             </Link>
           ) : (
             <Link href={"#"} key={link.text + link.index}>
-              <p>{link.text}</p>
+              <p className={Nav_module.text}>{link.text}</p>
             </Link>
           )
         )}
@@ -34,6 +37,23 @@ export default function Nav() {
           <IconBtn icon={"shopping_bag"} />
         </div>
       </div>
+      {/* <div className={Nav_module.main}>
+        {links.map((link: any) =>
+          link.src ? (
+            <Link href={"#"} key={link.text + link.index}>
+              <Image src={link.src} height={50} width={200} alt={link.text} />
+            </Link>
+          ) : (
+            <Link href={"#"} key={link.text + link.index}>
+              <p className={Nav_module.text}>{link.text}</p>
+            </Link>
+          )
+        )}
+        <div className={Nav_module.icons}>
+          <IconBtn icon={"person_outline"} />
+          <IconBtn icon={"shopping_bag"} />
+        </div>
+      </div> */}
     </div>
   );
 }
