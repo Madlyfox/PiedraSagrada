eef13a49085275949a015bf360cf2b650a856ca5
Borromée Stanley <borromee.stanley@gmail.com>
2024-08-26 18:41:41 +0200

    pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 const path = require("path");
 
 module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  output: "export",
+  basePath: "/nextjs-github-pages",
+
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
@@ -15,32 +24,3 @@ module.exports = {
     ],
   },
 };
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /**
-   * Enable static exports for the App Router.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
-  output: "export",
-
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/nextjs-github-pages",
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true,
-  },
-};
-
-export default nextConfig;
